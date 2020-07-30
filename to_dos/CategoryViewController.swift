0d3e42a10a40bbd34b322056f3660d5fe5fe6a8b
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-07-30 02:10:32 -0400

    inherite swipeview

--- to_dos/CategoryViewController.swift
+++ to_dos/CategoryViewController.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 import RealmSwift
-import SwipeCellKit
 
-class CategoryViewController: UITableViewController {
+
+class CategoryViewController: SwipeTableViewController {
     
     let realm = try! Realm()
 
@@ -21,7 +21,7 @@ class CategoryViewController: UITableViewController {
         
         loadCategories()
         
-        tableView.rowHeight = 80.0
+        
 }
 
     //MARK: - TableView Datesource Methods
@@ -30,19 +30,13 @@ class CategoryViewController: UITableViewController {
         
         return categories?.count ?? 1
     }
-//
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! SwipeTableViewCell
-//        cell.delegate = self
-//        return cell
-//    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! SwipeTableViewCell
+
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories added yet"
         
-        cell.delegate = self
         
         return cell
         
@@ -81,6 +75,19 @@ class CategoryViewController: UITableViewController {
         
         tableView.reloadData()
     }
+    //MARK: -DElete data from swipe
+    
+    override func updateModel(at indexPath: IndexPath){
+        if let categoryForDeletion = self.categories?[indexPath.row] {
+            do {
+                try self.realm.write{
+                    self.realm.delete(categoryForDeletion)
+            }
+            } catch {
+                print("Error deleting category, \(error)")
+            }
+        }
+    }
     
     //MARK: - add New Categories
     
@@ -109,34 +116,3 @@ class CategoryViewController: UITableViewController {
     
 }
 
-
-//MARK - Swipe Cell Delegate Methods
-extension CategoryViewController: SwipeTableViewCellDelegate{
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
-        guard orientation == .right else { return nil }
-        
-        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
-            //handle action by updating model with deletion
-            
-            if let categoryForDeletion = self.categories?[indexPath.row] {
-                do {
-                    try self.realm.write{
-                        self.realm.delete(categoryForDeletion)
-                }
-                } catch {
-                    print("Error deleting category, \(error)")
-                }
-            }
-        }
-        //customize the action appearance
-        deleteAction.image = UIImage(named: "delete-icon")
-        
-        return [deleteAction]
-    }
-    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
-        var options = SwipeOptions()
-        options.expansionStyle = .destructive
-        return options
-    }
-    
-}
--- to_dos/to_dos/Controllers/ToDoListVIewController.swift
+++ to_dos/to_dos/Controllers/ToDoListVIewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RealmSwift
 
-class ToDoListViewController: UITableViewController {
+class ToDoListViewController: SwipeTableViewController {
 
     var todoItems: Results<Item>?
     let realm = try! Realm()
@@ -37,7 +37,7 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         if let item = todoItems?[indexPath.row] {
             
@@ -128,7 +128,20 @@ class ToDoListViewController: UITableViewController {
         todoItems = selectedCategory?.items.sorted(byKeyPath: "title", ascending: true)
         tableView.reloadData()
     }
-}
+    
+    override func updateModel(at indexPath: IndexPath) {
+        if let item = todoItems?[indexPath.row] {
+            do{
+            try realm.write{
+                realm.delete(item)
+                }
+                } catch {
+                    print("Error deleteing item, \(error)")
+                }
+            }
+        }
+    }
+
 
 // MARK: - Search bar methods
 
